15692f0091a66fcdf3600a73a1d868a7b0c8fceb
jesteban39 <juanesqb28@gmail.com>
2024-02-04 19:25:49 -0500

    test watch

--- jest.config.ts
+++ jest.config.ts
@@ -5,8 +5,12 @@ import type {Config} from '@jest/types'
 const jestConfig: Config.InitialOptions = {
   verbose: true,
   testTimeout: 90000,
+  watch: false,
+  watchAll: false,
   detectOpenHandles: true,
+  forceExit: true,
   preset: 'ts-jest',
+  roots: ['<rootDir>/src/', '<rootDir>/tests/'],
   testEnvironment: 'node',
   testPathIgnorePatterns: ['dist/', 'node_modules/', 'coverage/'],
   modulePaths: [compilerOptions.baseUrl],
